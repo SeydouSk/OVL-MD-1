54a05fe223d8adfef909cfe578537b3a45ef11b6
Ousmane9398 <ousmvne38@gmail.com>
2025-04-09 05:29:39 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
+  PREFIXE: process.env.PREFIXE || ".",
+  NOM_OWNER: process.env.NOM_OWNER || "•~•",
   NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "privé",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_wb8j6Nbc_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Gros chyeng",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "...",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
